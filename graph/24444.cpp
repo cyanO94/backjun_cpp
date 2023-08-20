b3e584fcc6096a06e0c5ed74f38c9c8c0f98f55f
cyan094 <dhdnjscks@gmail.com>
2023-08-21 00:10:46 +0900

    .

---
+++ graph/24444.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+vector<vector<int>> graph;
+vector<int> visited;
+int cnt = 1; 
+
+queue<int> q;
+void bfs() {
+    int node;
+    while (!q.empty()) {
+        node = q.front();
+        q.pop();
+
+        if (visited[node]) {
+            continue;
+        }
+        visited[node] = cnt;
+        cnt += 1;
+
+        for (int i = 0; i < graph[node].size(); i++) {
+            if (!visited[graph[node][i]]) {
+                q.push(graph[node][i]);
+            }
+        }
+    }
+
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0); 
+    cout.tie(0);
+
+    int n, m, r;
+    cin >> n >> m >> r;
+    int s, e;
+    graph.assign(n + 1, vector<int>());
+    visited = vector(n + 1, 0);
+
+    for (int i = 0; i < m; i++) {
+        cin >> s >> e;
+        graph[s].push_back(e);
+        graph[e].push_back(s);
+    }
+
+    for (int i = 0; i < graph.size(); i++) {
+        sort(graph[i].begin(), graph[i].end());//, compare);
+    }
+
+    q.push(r);
+    bfs();
+
+    for (int i = 1; i < visited.size(); i++) {
+            cout << visited[i] << "\n";
+    }
+
+
+    return 0;
+}
